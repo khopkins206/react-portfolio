416585642154be89a97cc4190641e1bcf037f6c8
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-19 18:12:17 -0500

    added routes

--- react-portfolio/client/src/App.js
+++ react-portfolio/client/src/App.js
@@ -1,22 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Homepage} />
+        {/* <Route exact path="/discover" component={Discover} /> */}
+        {/* <Route exact path="/search" component={Search} /> */}
+        {/* <Route exact path="/profile" component={Profile} /> */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
